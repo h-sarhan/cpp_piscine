41ac6200213abd60f70f5dd7e4066bb24b2f2645
Hassan Sarhan <hassanAsarhan@outlook.com>
2022-09-09 08:06:08 +0400

    Complete cpp m01 ex02

---
+++ module01/ex02/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hsarhan <hsarhan@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/09 07:59:13 by hsarhan           #+#    #+#             */
+/*   Updated: 2022/09/09 08:04:44 by hsarhan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+#include <iostream>
+
+int main()
+{
+	std::string	str = "HI THIS IS BRAIN";
+	std::string *strPTR = &str;
+	std::string &strREF = str; 
+	
+	std::cout << "str address: " << &str << std::endl;
+	std::cout << "strPTR address: " << strPTR << std::endl;
+	std::cout << "strREF address: " << &strREF << std::endl;
+	std::cout << std::endl;
+	
+	std::cout << "str value: " << str << std::endl;
+	std::cout << "strPTR value: " << *strPTR << std::endl;
+	std::cout << "strREF value: " << strREF << std::endl;
+}
