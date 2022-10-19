20bd02db923314fe0371a5b9d60bd2bd576802d5
Hassan Sarhan <hassanAsarhan@outlook.com>
2022-10-19 21:59:30 +0400

    Fixed ex00 module08

--- module08/ex00/easyfind.hpp
+++ module08/ex00/easyfind.hpp
@@ -6,21 +6,21 @@
 /*   By: hsarhan <hsarhan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 14:19:47 by hsarhan           #+#    #+#             */
-/*   Updated: 2022/10/17 15:11:49 by hsarhan          ###   ########.fr       */
+/*   Updated: 2022/10/19 21:57:31 by hsarhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASY_FIND
 #define EASY_FIND
 
-#include <algorithm>
 #include <iterator>
+#include <algorithm>
 #include <stdexcept>
 
-template <typename T> typename T::iterator easyfind(T nums, int toFind)
+template <typename T> typename T::iterator easyfind(T &nums, int toFind)
 {
-    typename T::iterator res = find(begin(nums), end(nums), toFind);
-    if (res == end(nums))
+    typename T::iterator res = std::find(nums.begin(), nums.end(), toFind);
+    if (res == nums.end())
         throw std::invalid_argument("Argument not found");
     return (res);
 }
--- module08/ex00/main.cpp
+++ module08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hsarhan <hsarhan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 14:19:52 by hsarhan           #+#    #+#             */
-/*   Updated: 2022/10/17 15:13:08 by hsarhan          ###   ########.fr       */
+/*   Updated: 2022/10/19 21:57:55 by hsarhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,13 @@ int	main(void)
 	std::vector<int> numVec(10, 42);
 	numVec[1] = 12;
 	numVec[2] = 54;
-	std::vector<int>::iterator res = easyfind(numVec, 12);
-	std::cout << *res << std::endl;
-	res++;
-	std::cout << *res << std::endl;
+	try
+	{
+		std::vector<int>::iterator res = easyfind(numVec, 12);
+		std::cout << *res << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 }
