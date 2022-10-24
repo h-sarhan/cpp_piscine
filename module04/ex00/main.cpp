392f628b0071eff8da0b4c4d1121cf9a3ede9409
Hassan Sarhan <hassanAsarhan@outlook.com>
2022-10-24 12:23:02 +0400

    Whatevet

--- module04/ex00/main.cpp
+++ module04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hsarhan <hsarhan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 07:51:24 by hsarhan           #+#    #+#             */
-/*   Updated: 2022/09/28 16:12:25 by hsarhan          ###   ########.fr       */
+/*   Updated: 2022/10/24 12:20:06 by hsarhan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ int main()
 	const WrongAnimal *meta = new WrongAnimal();
 	// const WrongAnimal *j = new Dog();
 	const WrongAnimal *i = new WrongCat();
-	
+
 	// std::cout << j->getType() << " " << std::endl;
 	std::cout << i->getType() << " " << std::endl;
 	i->makeSound();
